2985dd66d1f932dc0c2477c9337ab4e41165088f
eulerlobo <eulerlobo@outlook.com>
2017-06-13 21:03:53 -0300

    Creation of de sqlite for the crawler

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import scrapy
+import sqlite3
 
 from datetime import datetime
 from service.seeds import seeds
@@ -8,23 +9,47 @@ from crawlerBitConfiguration import crawlerBitConfiguration
 class CrawlerBit(scrapy.Spider):
 
     TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
-
+        
     name = crawlerBitConfiguration.getCrawlerName()
     start_urls = seeds.getSeeds()
 
     def parse(self, response):
         url = response.url,
+        url = url[0]
+
         title = response.xpath("//title/text()").extract_first()
         time  = datetime.now().strftime(CrawlerBit.TIME_FORMAT)
-        content = response.xpath("//body").extract(),
+
+        content = ''.join(response.xpath("//body").extract()),
+        content = content[0]
 
         #TODO: Save elements in database
+        #https://stackoverflow.com/questions/22799990/beatifulsoup4-get-text-still-has-javascript
 
-        yield {
-            "url": response.url,
-            "title":  response.xpath("//title/text()").extract_first(),
-            "time": datetime.now().strftime(CrawlerBit.TIME_FORMAT)
-        }
+        # GO HORSE ... GO!!!!
+
+        print("------------------------------------")
+        print (url)
+        print (title)
+        print (time)
+        #print (content)
+        print("------------------------------------")
+
+        database = sqlite3.connect('crawlerbit.db')
+        connection = database.cursor()
+        connection.execute("CREATE TABLE IF NOT EXISTS raw_crawler_data (id integer PRIMARY KEY AUTOINCREMENT, url text NOT NULL, content text NOT NULL, time datetime NOT NULL);")
+
+        connection.execute("INSERT INTO raw_crawler_data (url, content, time) VALUES (?, ?, ?)", (url, content, time))
+        database.commit()
+
+        connection.close()
+        database.close()
+
+        # yield {
+        #    "url": response.url,
+        #    "title":  response.xpath("//title/text()").extract_first(),
+        #    "time": datetime.now().strftime(CrawlerBit.TIME_FORMAT)
+        # }
 
         extractor = LinkExtractor()
         links = extractor.extract_links(response)
